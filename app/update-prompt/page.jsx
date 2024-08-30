96206bdf3bb5dca811d4a56ce876cfe8b34f5d94
Symbol <Symbolistic@live.com>
2024-08-30 00:30:18 -0400

    trying to fix update prompt

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Suspense } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 
 import Form from '@components/Form';
@@ -60,13 +60,15 @@ const EditPrompt = () => {
 	};
 
 	return (
-		<Form
-			type='Edit'
-			post={post}
-			setPost={setPost}
-			submitting={submitting}
-			handleSubmit={updatePrompt}
-		/>
+		<Suspense fallback={<div>Loading...</div>}>
+			<Form
+				type='Edit'
+				post={post}
+				setPost={setPost}
+				submitting={submitting}
+				handleSubmit={updatePrompt}
+			/>
+		</Suspense>
 	);
 };
 
